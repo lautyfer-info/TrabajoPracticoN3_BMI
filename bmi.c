7c4d5dc4709808f80e4ef18953a3b5ccfc0dd334
lautyfer-info <lautyfer0089@gmail.com>
2026-07-03 14:17:14 -0300

    TP4-pt3
    Codigo modificado para pt3 de TP4

--- bmi.c
+++ bmi.c
@@ -10,8 +10,17 @@ int main(int argc, char *argv[]) {
 	imc= (p/(a*a));
 	
 	printf("\nSu IMC es: %.2f\n", imc);
-	printf("\nIndice    |  Condicion\n -------------------\n  <18.5  | bajo peso\n 18.5 a 24.9 | Normal\n 25.0 a 29.9 | Sobrepeso\n >=30 | Obesidad");
+	printf("\nIndice    |  Condicion\n -------------------\n  <18.5  | bajo peso\n 18.5 a 24.9 | Normal\n 25.0 a 29.9 | Sobrepeso\n >=30 | Obesidad\n");
 	
+	
+	if(imc < 18.5){
+		printf("\nUsted esta: Bajo de peso");}
+	else if(imc >= 18.5 && imc <= 24.9){
+		printf("\nUsted esta: Normal");}
+	else if(imc >= 25.0 && imc <= 29.9){
+		printf("\nUsted tiene: Sobrepeso");}
+	else if(imc >= 30){
+		printf("\nUsted tiene: Obesidad");}
 	return 0;
 }
 
